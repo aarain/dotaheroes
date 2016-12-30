f4651301a59b14db79751b4cda110d74152c8196
valerijf <valerijf@stud.ntnu.no>
2016-12-30 15:03:19 +0100

    Moved activities to fragments

--- app/src/main/java/com/freva/dotaheroes/MainActivity.java
+++ app/src/main/java/com/freva/dotaheroes/MainActivity.java
@@ -1,25 +1,13 @@
 package com.freva.dotaheroes;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
-import com.freva.dotaheroes.R;
-import com.freva.dotaheroes.adapter.HeroListingAdapter;
-import com.freva.dotaheroes.container.Hero;
-
-import com.freva.dotaheroes.data.Heroes;
-
-import java.util.Collections;
-import java.util.List;
+import com.freva.dotaheroes.fragment.HeroListing;
 
 public class MainActivity extends AppCompatActivity {
     private static MainActivity mainActivity;
-    private HeroListingAdapter heroListingAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Hero> heroes = Heroes.getHeroes();
-        Collections.sort(heroes, (o1, o2) -> o1.getName().compareTo(o2.getName()));
-
-        heroListingAdapter = new HeroListingAdapter(getApplicationContext(), heroes);
-        ListView listview = (ListView) findViewById(R.id.heroes_list_view);
-        listview.setAdapter(heroListingAdapter);
-        listview.setOnItemClickListener((parent, view, position, id) -> {
-            Hero selectedHero = ((Hero) parent.getItemAtPosition(position));
-            Intent myIntent = new Intent(view.getContext(), HeroDetailsActivity.class);
-            myIntent.putExtra("HERO_ID", selectedHero.getId());
-            startActivityForResult(myIntent, 0);
-        });
-
-        EditText heroSearch = (EditText) findViewById(R.id.activity_main_hero_search_input);
-        heroSearch.addTextChangedListener(new FilterHeroListener());
-    }
-
-    private class FilterHeroListener implements TextWatcher {
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            heroListingAdapter.getFilter().filter(s);
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-
-        @Override
-        public void afterTextChanged(Editable s) { }
+        Fragment fragment = new HeroListing();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.activity_main_frame_container, fragment)
+                .commit();
     }
 
     public static MainActivity getMainActivity() {
---
+++ app/src/main/java/com/freva/dotaheroes/fragment/HeroListing.java
@@ -0,0 +1,69 @@
+package com.freva.dotaheroes.fragment;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.freva.dotaheroes.R;
+import com.freva.dotaheroes.adapter.HeroListingAdapter;
+import com.freva.dotaheroes.container.Hero;
+
+import com.freva.dotaheroes.data.Heroes;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HeroListing extends Fragment {
+    private HeroListingAdapter heroListingAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_hero_listing, container, false);
+
+        List<Hero> heroes = Heroes.getHeroes();
+        Collections.sort(heroes, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+
+        heroListingAdapter = new HeroListingAdapter(getActivity().getApplicationContext(), heroes);
+        ListView listview = (ListView) rootView.findViewById(R.id.fragment_hero_listing_list_view);
+        listview.setAdapter(heroListingAdapter);
+        listview.setOnItemClickListener((parent, view, position, id) -> {
+            Hero selectedHero = ((Hero) parent.getItemAtPosition(position));
+
+            Bundle bundle = new Bundle();
+            bundle.putString("HERO_ID", selectedHero.getId());
+            Fragment fragment = new HeroDetails();
+            fragment.setArguments(bundle);
+
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.activity_main_frame_container, fragment)
+                    .addToBackStack(selectedHero.getId())
+                    .commit();
+        });
+
+        EditText heroSearch = (EditText) rootView.findViewById(R.id.fragment_hero_listing_search_input);
+        heroSearch.addTextChangedListener(new FilterHeroListener());
+
+        return rootView;
+    }
+
+    private class FilterHeroListener implements TextWatcher {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            heroListingAdapter.getFilter().filter(s);
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+    }
+}
