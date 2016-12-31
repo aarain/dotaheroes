fd2ba799b0bcfc6b410caf9cef838c82d32873cc
ASHLEY <ashleyarain@hotmail.com>
2016-12-31 16:10:29 +0000

    PogChamp abilities

--- app/src/main/java/com/freva/dotaheroes/fragment/HeroDetailsAbilities.java
+++ app/src/main/java/com/freva/dotaheroes/fragment/HeroDetailsAbilities.java
@@ -20,7 +20,7 @@ public class HeroDetailsAbilities extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View rootView = inflater.inflate(R.layout.fragment_hero_abilities, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_hero_details_abilties, container, false);
 
         Hero selectedHero = Heroes.getHero(getArguments().getString("HERO_ID"));
 
