d2c1386f92cd09444eef51fc96832d7752ca3e77
ASHLEY <ashleyarain@hotmail.com>
2017-01-01 11:12:47 +0000

    graphical changes

--- app/src/main/java/com/freva/dotaheroes/adapter/HeroAbilitiesAdapter.java
+++ app/src/main/java/com/freva/dotaheroes/adapter/HeroAbilitiesAdapter.java
@@ -41,10 +41,11 @@ public class HeroAbilitiesAdapter extends ArrayAdapter<Ability> {
             viewHolder = new ViewHolder();
             viewHolder.name = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_name);
             viewHolder.icon = (ImageView) convertView.findViewById(R.id.fragment_hero_details_ability_icon);
-            viewHolder.affects = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_affects);
-            viewHolder.desc = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_description);
             viewHolder.mana = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_mana);
             viewHolder.cooldown = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_cooldown);
+            viewHolder.desc = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_description);
+            viewHolder.affects_1 = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_affects_1);
+            viewHolder.affects_2 = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_affects_2);
             viewHolder.details = (TextView) convertView.findViewById(R.id.fragment_hero_details_ability_details);
 
             convertView.setTag(viewHolder);
@@ -55,21 +56,34 @@ public class HeroAbilitiesAdapter extends ArrayAdapter<Ability> {
         viewHolder.name.setText(ability.getName());
         viewHolder.icon.setImageResource(ability.getIconResourceIdentifier());
 
-        List<String> keys = new ArrayList<>(ability.getAffects().keySet());
-        Collections.sort(keys);
-        StringBuilder sbAffects = new StringBuilder();
-        for (String key: keys) {
-            String htmlDetails = String.format(mapFormat, key, ability.getAffects().get(key));
-            sbAffects.append(htmlDetails);
-        }
-        viewHolder.affects.setText(Html.fromHtml(sbAffects.toString()));
-        viewHolder.desc.setText(ability.getDescription());
-
         String manaText = ability.getMana() != null ? ability.getMana() : "N/A";
         viewHolder.mana.setText(Html.fromHtml("<b>MANA COST:</b> " + manaText));
 
         String cooldownText = ability.getCooldown() != null ? ability.getCooldown() : "N/A";
         viewHolder.cooldown.setText(Html.fromHtml("<b>COOLDOWN:</b> " + cooldownText));
+
+        viewHolder.desc.setText(ability.getDescription());
+
+        List<String> keys = new ArrayList<>(ability.getAffects().keySet());
+        Collections.sort(keys);
+        StringBuilder sbAffects = new StringBuilder();
+        for (String key: keys) {
+            if (key == "PIERCES SPELL IMMUNITY"){
+                if (ability.getAffects().get(key).equals("Yes")) {
+                    String psi = String.format("<font color=\"green\">%s</font>", key);
+                    viewHolder.affects_2.setText(Html.fromHtml(psi));
+                }
+                else if (ability.getAffects().get(key).equals("No")) {
+                    String psi = String.format("<font color=\"red\"><del>%s</del></font>", key);
+                    viewHolder.affects_2.setText(Html.fromHtml(psi));
+                }
+            }
+            else {
+                String htmlDetails = String.format(mapFormat, key, ability.getAffects().get(key));
+                sbAffects.append(htmlDetails);
+            }
+        }
+        viewHolder.affects_1.setText(Html.fromHtml(sbAffects.toString()));
 
         StringBuilder sbDetails = new StringBuilder();
         for (Map.Entry<String, String> entry : ability.getDetails().entrySet()) {
@@ -100,13 +114,14 @@ public class HeroAbilitiesAdapter extends ArrayAdapter<Ability> {
         // First row
         private TextView name;
         // Second row
-        private ImageView icon;
-        private TextView affects;
+        private ImageView icon;             // First column
+        private TextView mana;              // Second column, First sub-row
+        private TextView cooldown;          // Second column, Second sub-row
         // Third row
         private TextView desc;
         // Fourth row
-        private TextView mana;
-        private TextView cooldown;
+        private TextView affects_1;         // First Column
+        private TextView affects_2;         // Second Column
         // Fifth row
         private TextView details;
     }
